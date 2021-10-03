68c682f7d2aea312d4702ea26381ace2841a1262
Joe Hall <joe.ithelp@gmail.com>
2021-10-03 10:09:56 -0400

    added notes for what I need to do next

--- src/components/GameComponent.jsx
+++ src/components/GameComponent.jsx
@@ -13,10 +13,25 @@ const Story = (props) => {
     );
 }
 
-const Actions = (props) => {
+//***Thought: do i need seperate functions one to display the action list and another for when a action is clicked?
+const Actions = (props) => { //***ToDo: Need to iterate through action object to create UL of all actions
+
+    const actionChoices = props.data[props.currentSection].action.map(actionOption => { //***Thought: this may need to be moved to the default component (see playercomponent for example)
+        return (
+            <div key={actionOption.id} className="col-sm-4">
+                <RenderAdventurerOptions adventurer={playerOption} onClick={props.onClick} />
+            </div>
+        )
+    });
+
     return (
         <React.Fragment>
             <h4>Actions:</h4>
+            <ul>
+                <li>
+                    {props.data[props.currentSection].action}
+                </li>
+            </ul>
         </React.Fragment>
     );
 }
@@ -34,20 +49,35 @@ class GameComponent extends Component {
     };
 
     progressGame = () => {
-        this.setState({ gameProgression: this.state.gameProgression + 1})
-    }
+        this.setState({ gameProgression: this.state.gameProgression + 1});
+    };
+
+    actionChoice = (choice) => {
+        this.setState({ chosenAction: choice});
+    };
 
     render() {
-        return (
+        return ( //***ToDo: create cards inside col div's for adventurer, actions, and story (maybe have a bottom card for interactions?)
             <React.Fragment>
                 <div className="container">
                     <div className="row justify-content-around">
                         <div className="characterActions col-5">
                             <h4>{this.props.adventurer.name}</h4>
-                            <Actions />
+                            <Actions
+                                data={this.state.gamedata}
+                                storyProgress={this.state.currentStory}
+                                currentSection={this.state.gameProgression}
+                                actionChoice={this.actionChoice}
+                                progressStory={this.progressStory}
+                                progressGame={this.progressGame}
+                            />
                         </div>
                         <div className="story col-5">
-                            <Story data={this.state.gamedata} storyProgress={this.state.currentStory} currentSection={this.state.gameProgression} />
+                            <Story
+                                data={this.state.gamedata}
+                                storyProgress={this.state.currentStory}
+                                currentSection={this.state.gameProgression}
+                            />
                         </div>
                     </div>
                 </div>
